de166fde299419ac4cda4499a1c905233ebf135e
mig29-62 <12584@holbertonstudents.com>
2025-12-04 15:51:06 +0400

    holberton

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,5 @@
 """we create a square class in this module"""
 class Square:
     """this is the class"""
-    def __init__(self,size):
-        self.__size=size
+    def __init__(self, size):
+        self.__size = size
