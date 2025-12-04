984c595781d87cb9766bb214c884a7f41d1a5d67
mig29-62 <12584@holbertonstudents.com>
2025-12-04 16:41:39 +0400

    holberton

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""we define the class square and raise errors about size"""
+
+
+class Square:
+    """specific errors have specific outputs"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return __size**2
