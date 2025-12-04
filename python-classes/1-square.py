d39298cac0a75a528feb23e0b14ce7ad18ee611b
mig29-62 <12584@holbertonstudents.com>
2025-12-04 16:34:05 +0400

    holberton

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-    """we define specific errors and according to that print specific messages"""
+    """specific errors have specific outputs"""
 
     def __init__(self, size=0):
-        if not isinstance(size,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size=size
+            self.__size = size
