843602b0e2c03236e15b399ef5b169c582e2ea88
mig29-62 <12584@holbertonstudents.com>
2025-12-05 04:09:43 +0400

    holberton

---
+++ python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""we define the class square and raise errors about size"""
+
+
+class Square:
+    """specific errors have specific outputs"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = size
+
+    @property
+    def size(self):
+        if not isinstance(self._size, int):
+            raise TypeError("size must be an integer")
+        else:
+            return self._size
+
+    def area(self):
+        return self._size**2
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(self._size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self._size = value
+
+    def my_print(self):
+        for i in range(0,self.area()):
+            for j in range(0,self.area()):
+                print('#',end='')
+            print('\n',end='')
